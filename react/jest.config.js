2ac2b28f2cc6124b9b88d6f6b9abd5632b3c71f4
Nando Vieira <me@fnando.com>
2019-11-20 23:01:03 -0800

    Add jest configuration for react.

---
+++ react/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.(jpe?g|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "jest-filename-transform",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["js", "ts", "tsx", "json"],
+};
